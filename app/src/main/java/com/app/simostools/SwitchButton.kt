a95e83018c293edc9b076c81ddb2af16c52bb303
Switchleg1 <switchleg1@gmail.com>
2021-11-08 07:20:57 -0700

    press buttons now change color

--- app/src/main/java/com/app/simostools/SwitchButton.kt
+++ app/src/main/java/com/app/simostools/SwitchButton.kt
@@ -17,8 +17,14 @@ class SwitchButton: androidx.appcompat.widget.AppCompatTextView {
     }
 
     private var mRect       = RectF(0f, 0f, 0f,0f)
+    private var mPressed    = false
     var margin              = 10f
     var round               = 50f
+    val paintBGPress: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style       = Paint.Style.FILL
+        color       = Color.GRAY
+        strokeCap   = Paint.Cap.ROUND
+    }
     val paintBG: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style       = Paint.Style.FILL
         color       = Color.LTGRAY
@@ -37,7 +43,11 @@ class SwitchButton: androidx.appcompat.widget.AppCompatTextView {
     }
 
     override fun onDraw(canvas: Canvas) {
-        canvas.drawRoundRect(mRect, round, round, paintBG)
+        if(mPressed) {
+            canvas.drawRoundRect(mRect, round, round, paintBGPress)
+        } else {
+            canvas.drawRoundRect(mRect, round, round, paintBG)
+        }
         canvas.drawRoundRect(mRect, round, round, paintRim)
         super.onDraw(canvas)
     }
@@ -46,5 +56,15 @@ class SwitchButton: androidx.appcompat.widget.AppCompatTextView {
         mRect.set(margin, margin, width.toFloat()-margin, height.toFloat()-margin)
 
         invalidate()
+    }
+
+    override fun performClick(): Boolean {
+        mPressed = true
+        postDelayed({
+            mPressed = false
+            invalidate()
+        }, 100)
+        invalidate()
+        return super.performClick()
     }
 }
